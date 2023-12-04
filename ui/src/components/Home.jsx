b31a82ce8ee6d35fc19745f8d38127061072fc46
vvssgowtham <gowthamvvss@gmail.com>
2023-12-04 16:45:01 +0530

    made some changes in home jsx

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <section>
       <div className="container mx-auto px-100">
-        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <header className="d-flex flex-wrap align-items-center justify-content-between py-3 mb-4 border-bottom">
           <div className="col-md-3 mb-2 mb-md-0">
             <a
               href="/"
@@ -24,33 +24,35 @@ function Home() {
               </svg>
             </a>
           </div>
-          <ul className="nav nav-pills flex-grow-1">
-            <li className="nav-item">
-              <a href="/allblogs" className="btn btn-outline-primary me-2">
-                AllBlogs
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#about" className="btn btn-outline-primary me-2">
-                About
-              </a>
-            </li>
-          </ul>
-          <div className="col-md-3 text-md-end mt-2 mt-md-0">
-            <button
-              type="button"
-              className="btn btn-outline-primary me-2"
-              onClick={() => navigate("/login")}
-            >
-              Login
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-primary me-2"
-              onClick={() => navigate("/signup")}
-            >
-              Sign-up
-            </button>
+          <div className="d-flex flex-column flex-md-row">
+            <ul className="nav nav-pills me-md-2">
+              <li className="nav-item">
+                <a href="/allblogs" className="btn btn-outline-primary me-2">
+                  AllBlogs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#about" className="btn btn-outline-primary me-2">
+                  About
+                </a>
+              </li>
+            </ul>
+            <div className="col-md-3 text-end">
+              <button
+                type="button"
+                className="btn btn-outline-primary me-2"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+              <button
+                type="button"
+                className="btn btn-outline-primary me-2"
+                onClick={() => navigate("/signup")}
+              >
+                Sign-up
+              </button>
+            </div>
           </div>
         </header>
 
