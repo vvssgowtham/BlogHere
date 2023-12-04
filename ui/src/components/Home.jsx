c65b4ca782816d5232a0d88d8f04fb67c9d9f379
vvssgowtham <gowthamvvss@gmail.com>
2023-12-04 17:13:50 +0530

    small commit

--- ui/src/components/Home.jsx
+++ ui/src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     <section>
       <div className="container mx-auto px-100">
         <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <div className="col-md-3 mb-2 mb-md-0">
+          <div className="col-md-3 p-2 mb-md-0">
             <a
               href="/"
               className="d-inline-flex link-body-emphasis text-decoration-none"
