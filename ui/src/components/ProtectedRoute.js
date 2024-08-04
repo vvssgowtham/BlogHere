982ff6028d68503a0bf21527454451b562fa4627
vvssgowtham <gowthamvvss@gmail.com>
2024-08-04 22:52:22 +0530

    chore: Add authentication check to ProtectedRoute component

--- ui/src/components/ProtectedRoute.js
+++ ui/src/components/ProtectedRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
+// eslint-disable-next-line react/prop-types
 const ProtectedRoute = ({ element }) => {
   const isAuthenticated = !!sessionStorage.getItem('token');
 
